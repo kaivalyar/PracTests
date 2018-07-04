770b52f8a5f11941948df52727a3585e625307b6
Kaivalya Rawal <kaivalyaraw@gmail.com>
2018-07-04 14:09:28 +0530

    System test (for Logic module)

---
+++ GenericSystemTest/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+PYTHONPATH="${PYTHONPATH}:/home/kaivalya/Documents/WORK/GSoC/kPracMLN/python3/"
+
+source ~/.prac3/bin/activate
+python3 test.py
+deactivate
+
+
+
+
+PYTHONPATH=""
+
+source ~/p3/bin/activate
+python3 test.py
+deactivate
+
+
+
+
+PYTHONPATH="${PYTHONPATH}:/home/kaivalya/Documents/WORK/GSoC/kPracMLN/python3/"
+
